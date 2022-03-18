6eb4e25603b61ed20a8c3790beed1df463a049b9
minji-ulalalab <kmj@ulalalab.com>
2022-03-18 13:41:26 +0900

    last version 1. 클라이언트-서버가 wiconL-wimX라 가정하고 테트스 프로그램 작성 2. 각 기능을 모듈화하여 import처리 3. 모듈을 package화 후 각 모듈 내부를 class로 변경 4. class명이 파이썬 내부 키워드와 겹치지 않도록 변경 5. 클라이언트가 서버에서 보낸 응답을 출력할 수 있도록 변경(client에 recv기능 추가)

---
+++ to_testserver/version4/module_package/count_m.py
@@ -0,0 +1,12 @@
+from queue import Queue
+import time
+
+class Count:
+    def run(self, q):
+        number = 0
+        while True:
+            if number > 65535:  #65535가되면 0으로 돌아간다
+                number = 0
+            q.put(number)
+            number += 1 # 1씩 증가
+            time.sleep(1) #1초 마다
---
+++ to_testserver/version4/module_package/receiving_m.py
@@ -0,0 +1,17 @@
+#wicon에서 오는 정보를 받는함수  
+class test_receiving:
+    def recv(self, sock):
+        while True:
+            try:
+                recvData = sock.recv(1024)
+                print(recvData.decode('utf-8'))
+            except:
+                print("연결 안됨")
+                break
+
+    
+
+        
+        
+
+        
